307396da371c7cc81b643cfcd8c88991d2a0cc4e
Igormandello <igormandello@gmail.com>
2018-02-17 16:29:17 -0200

    Implements printSpots method
    - printSpots returns an array with divs, each one created by
    createSpotBox
    
    - A spotBox div have the message sent, and two unimplemented buttons, to
    approve or reject the spot

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import yawp from 'yawp';
 import './App.css';
 
+import approveIcon from './imgs/approve.png';
+import rejectIcon from './imgs/reject.png';
 
 class App extends Component 
 {
   constructor(props)
   {
     super(props);
+    
+    yawp.config(function (c) {
+      c.baseUrl('http://new-spotted-cotuca.appspot.com/api');
+    });
+    
+    yawp('/spots/pending').list(l => this.setState({spots: l}));
   };
 
   state =
@@ -18,8 +26,38 @@ class App extends Component
   printSpots()
   {
     let spotsDivs =[];
+    this.state.spots.forEach(spot => 
+    {
+      spotsDivs.push(this.createSpotBox(spot));
+    });
+    
     return spotsDivs;
   }
+
+  createSpotBox(spot)
+  {
+    return (
+      <div className="spotBox"> 
+        { "\"" + spot.message + "\"" } 
+        
+        <hr/>
+        
+        <div className="spotBoxFooter">
+          <img alt="approve" className="changeStatus" src={ approveIcon } onClick={ () => this.approveSpot(spot.id) }></img>
+          <img alt="reject" className="changeStatus" src={ rejectIcon } onClick={ () => this.rejectSpot(spot.id) }></img>
+        </div>
+      </div>
+    );
+  }
+
+  approveSpot(id)
+  {
+  }
+
+  rejectSpot(id)
+  {
+  }
+
   render() 
   {
     return (
