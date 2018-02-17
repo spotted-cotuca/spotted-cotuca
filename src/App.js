e74b61a6d5786175c31f4e1833f35ecce3effddc
Igormandello <igormandello@gmail.com>
2018-02-17 16:22:35 -0200

    Removes some default elements
    - Deletes the React logo and related CSS classes
    
    - Removes default messages
    
    - Adds the content div

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import yawp from 'yawp';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+class App extends Component 
+{
+  constructor(props)
+  {
+    super(props);
+  };
+
+  state =
+  {
+    spots: []  
+  }
+  
+  printSpots()
+  {
+    let spotsDivs =[];
+    return spotsDivs;
+  }
+  render() 
+  {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Spotted Cotuca</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <div className="content">
+          {
+            this.printSpots()
+          }
+        </div>
       </div>
     );
   }
