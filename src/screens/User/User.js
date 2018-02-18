d1127ba190ec8ff0958ec6900e305c520419f593
Igormandello <igormandello@gmail.com>
2018-02-17 23:14:32 -0300

    Changes the design of pages
    - Some common CSS classes now are in index.css
    
    - The color pallete of the site now is based on red

--- src/screens/User/User.js
+++ src/screens/User/User.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import yawp from 'yawp';
 import './User.css';
+import '../../index.css';
 
-import approveIcon from '../../imgs/approve.png';
-import rejectIcon from '../../imgs/reject.png';
+import letterIcon from '../../imgs/letter.png';
 
 class User extends Component 
 {
@@ -32,6 +32,7 @@ class User extends Component
     return (
       <div className="App">
         <header className="App-header">
+          <img className="letter" src={ letterIcon } alt="letter"></img>
           <h1 className="App-title">Spotted Cotuca</h1>
         </header>
         
@@ -40,7 +41,7 @@ class User extends Component
             Olá! Esse é o novo método de mandar spots no COTUCA :). Basta mandar a mensagem no campo abaixo e esperar ser aprovada por um de nossos administradores, para que ela seja postada no nosso Facebook oficial (nome da página) e no nosso twitter (@dospotted). Boa sorte com os crushes!
           </div>
           
-          <textarea maxLength="280" id="message"></textarea>
+          <textarea maxLength="280" placeholder="Digite sua mensagem..." id="message"></textarea>
           
           <br/>
           
