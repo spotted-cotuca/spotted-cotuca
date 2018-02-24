0e4b4bf864374f28b33a8d15072b37e5d454eb45
Igormandello <igormandello@gmail.com>
2018-02-24 16:42:46 -0300

    Ends corrections

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -93,7 +93,7 @@ class User extends Component
         <a href="./send"><img alt="sendSpot" className="sendSpot" src={ messageIcon }/></a>
           
         <div className="App-footer">
-          Feito com <i className="heart">♥</i> por <a className="fbLink" href="https://fb.com/igor.mandello" target="blank">Igor</a> e <a className="fbLink" href="https://fb.com/lorenzopincinato" target="blank">Lorenzo</a>
+            Feito com <i className="heart">♥</i> por <a className="fbLink" href="https://fb.com/igor.mandello" target="blank">Igor</a> e <a className="fbLink" href="https://fb.com/lorenzopincinato" target="blank">Lorenzo</a>
         </div>
       </div>
     );
