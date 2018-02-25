86ccec8168558b7099d499e186297ca78fd264db
Igormandello <igormandello@gmail.com>
2018-02-25 04:23:37 -0300

    Corrects word-wrapping problem

--- src/screens/User/User.js
+++ src/screens/User/User.js
@@ -28,7 +28,19 @@ class User extends Component
     let textArea = document.getElementById("message"),
         text = textArea.value;
     
-    if (textArea.value !== "")
+    if (text == "")
+      Alert.error(<h1>Se você não escrever nada, não tem como o crush te notar!</h1>, {
+        position: 'bottom-right',
+        effect: 'scale',
+        timeout: 4000
+      });
+    else if (text.length > 280)
+      Alert.error(<h1>Somos integrados com o Twitter, logo, não podemos aceitar spots com mais de 180 caracteres 😢</h1>, {
+          position: 'bottom-right',
+          effect: 'scale',
+          timeout: 4000
+      });
+    else
       yawp('/spots').create({ message: textArea.value }).then(() => 
       { 
         textArea.value = "";
@@ -38,13 +50,7 @@ class User extends Component
           this.createSuccessAlert("Sua mensagem foi enviada, E É CLARO QUE SEU CRUSH TE QUER!");
         else
           this.createSuccessAlert("Sua mensagem foi enviada, agora é só esperar!");
-      });
-    else
-      Alert.error(<h1>Se você não escrever nada, não tem como o crush te notar!</h1>, {
-            position: 'bottom-right',
-            effect: 'scale',
-            timeout: 4000
-        });
+      });  
   }
   
   createSuccessAlert(message)
@@ -65,17 +71,19 @@ class User extends Component
         </header>
         
         <div className="content">
-          <div className="presentation">
-            Olá! Esse é o novo método de mandar spots no COTUCA :). Basta mandar a mensagem no campo abaixo e esperar ser aprovada por um de nossos administradores, para que ela seja postada no nosso Facebook oficial (nome da página) e no nosso twitter (@dospotted). Boa sorte com os crushes!
+          <div className="middle">
+            <div className="presentation">
+              Olá! Esse é o novo método de mandar spots no COTUCA :). Basta mandar a mensagem no campo abaixo e esperar ser aprovada por um de nossos administradores, para que ela seja postada no nosso <a className="socialLink" href="https://fb.com/spottedcotuca3" target="blank">Facebook</a> e no nosso <a className="socialLink" href="https://twitter.com/spottedcotuca3" target="blank">Twitter</a>. Boa sorte com os crushes!
+            </div>
+
+            <textarea maxLength="280" placeholder="Digite sua mensagem..." id="message"></textarea>
+
+            <br/>
+
+            <button className="btn btn-primary" onClick={() => this.sendSpot()}>Enviar Spot</button>
+
+            <Alert stack={{limit: 3}} />
           </div>
-          
-          <textarea maxLength="280" placeholder="Digite sua mensagem..." id="message"></textarea>
-          
-          <br/>
-          
-          <button className="btn btn-primary" onClick={() => this.sendSpot()}>Enviar Spot</button>
-          
-          <Alert stack={{limit: 3}} />
         </div>
         
         <footer className="App-footer">
