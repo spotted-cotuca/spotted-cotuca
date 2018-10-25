3f33841b5ce75c607d2dfa941e35a88f4fa13c7e
Igormandello <igormandello@gmail.com>
2018-10-25 15:51:49 -0300

    Converts NotFound class into a functional component

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import '../css/NotFound.css';
 import '../css/index.css';
 
 import brokenHeartIcon from '../imgs/broken-heart.png';
 
-class NotFound extends Component 
-{
-  render() 
-  {
-    return (
-      <div className="content notFound">
-        <img className="brokenHeart" src={ brokenHeartIcon } alt="Broken Heart"></img>
-        <div className="message">
-          <strong>Oh, não!</strong>
-          <br/><br/>
+function NotFound() {
+  return (
+    <div className="content notFound">
+      <img className="brokenHeart" src={ brokenHeartIcon } alt="Broken Heart"></img>
+      <div className="message">
+        <strong>Oh, não!</strong>
+        <br/><br/>
           Parece que o crush não está nessa direção, aliás, <strong>NADA</strong> está nessa direção.
-          <br/><br/>
-
-          <a className="link" href="./#/">Voltar para a home.</a>
-        </div>
+        <br/><br/>
+        <a className="link" href="./#/">Voltar para a home.</a>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default NotFound;
